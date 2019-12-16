c34ef04fe917c620c6ca2c38dae00d4bc4ab0d6d
Mathias Haessig <cornholio127@live.com>
2019-12-16 10:42:44 +0100

    Build multiple services

--- scripts/gulpfile.ts
+++ scripts/gulpfile.ts
@@ -1,11 +1,6 @@
 import { src, dest, series, task, TaskFunction } from 'gulp';
 import { exec } from 'child_process';
 
-const SERVER = '../server';
-const DOCKER = '../docker';
-const PRODUCT_SERVICE = `${SERVER}/product-service`;
-const PRODUCT_SERVICE_DOCKER = `${DOCKER}/product-service`;
-
 const childProccessStdout = (s: string) => console.log(`\x1b[90m${s}\x1b[0m`);
 
 const cmd = (shellCommand: string, workingDirectory: string): TaskFunction => cb => {
@@ -15,11 +10,21 @@ const cmd = (shellCommand: string, workingDirectory: string): TaskFunction => cb
   cp.on('exit', () => cb());
 };
 
-task('buildProductService', cmd('npm run build', PRODUCT_SERVICE));
-task('copyProductServicePackageJson', () => src(`${PRODUCT_SERVICE}/package*.json`).pipe(dest(`${PRODUCT_SERVICE_DOCKER}/build`)));
-task('copyProductServiceDist', () => src(`${PRODUCT_SERVICE}/dist/**/*`).pipe(dest(`${PRODUCT_SERVICE_DOCKER}/build/dist`)));
-task('buildProductServiceDocker', cmd('docker build -t shop-product-service .', PRODUCT_SERVICE_DOCKER));
+const buildService = (serviceName: string): TaskFunction => {
+  const serviceNameCc = serviceName.split('-').map(n => n.substring(0, 1).toUpperCase() + n.substring(1)).join('');
+  const serviceProject = `../server/${serviceName}`;
+  const serviceDocker = `../docker/${serviceName}`;
+  const dockerImage = `shop-${serviceName}`;
+  task(`build${serviceNameCc}`, cmd('npm run build', serviceProject));
+  task(`copy${serviceNameCc}PackageJson`, () => src(`${serviceProject}/package*.json`).pipe(dest(`${serviceDocker}/build`)));
+  task(`copy${serviceNameCc}Dist`, () => src(`${serviceProject}/dist/**/*`).pipe(dest(`${serviceDocker}/build/dist`)));
+  task(`build${serviceNameCc}Docker`, cmd(`docker build -t ${dockerImage} .`, serviceDocker));
+  return series(`build${serviceNameCc}`, `copy${serviceNameCc}PackageJson`, `copy${serviceNameCc}Dist`, `build${serviceNameCc}Docker`);
+};
 
-const productService = series('buildProductService', 'copyProductServicePackageJson', 'copyProductServiceDist', 'buildProductServiceDocker');
+const buildProductService = buildService('product-service');
+const buildUserService = buildService('user-service');
 
-export default productService;
+const build = series(buildProductService, buildUserService);
+
+export default build;
